8699cccae82cae00dab3b21647263222f40b6138
ddang9390 <dnld1ng@yahoo.com>
2024-08-27 11:51:47 -0400

    More exrcises with javascript, focus on DOM

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,48 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(vals) {
+	let total = 0;
+  vals.forEach(val => {
+    total += val;
+  });
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(vals) {
+  let total = 1;
+  vals.forEach(val => {
+    total *= val;
+  });
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	let i = num2;
+  let total = num1;
+  while(i > 1){
+    total *= num1;
+    i--;
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if(num == 0){
+    return 1;
+  }
+	let total = num;
+  let i = num-1;
+  while(i > 0){
+    total *= i;
+    i--;
+  }
+  return total;
 };
 
 // Do not edit below this line
