6550aedd4dbfb6e9e975c2edc8d628a8ef4c83aa
ddang9390 <dnld1ng@yahoo.com>
2024-09-09 16:23:04 -0400

    Complete palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,15 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let reversed = "";
+    for(let i = (word.length)-1; i >= 0; i--){
+        reversed += (word.charAt(i));
+    }
+    let v1 = word.toLowerCase();
+    let v2 = reversed.toLowerCase();
+
+    v1 = v1.replace(/[^0-9a-z]/gi,'')
+    v2 = v2.replace(/[^0-9a-z]/gi,'')
+
+    return v1 == v2;
 
 };
 
