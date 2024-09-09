dce4fe5420fd5618496f8eee82cd43340256acfa
ddang9390 <dnld1ng@yahoo.com>
2024-09-09 16:31:08 -0400

    Completed fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = parseInt(num)
+    if(num == 0){
+        return 0;
+    }
+    if(num < 0){
+        return "OOPS";
+    }
+    if(num < 2){
+        return 1;
+    }
+    let numbers = [1, 1];
+    let index = 1;
 
+    while(index < num){
+        numbers.push(numbers[index] + numbers[index-1]);
+        index++;
+    }
+    return numbers[index-1];
 };
 
 // Do not edit below this line
